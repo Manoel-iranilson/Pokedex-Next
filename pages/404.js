6c26bcee7e6947a2e0fed7fb8e1e0dede5009a35
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-10-19 09:16:13 -0300

    pagina NotFound

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
-
+import styles from '../styles/404.module.css'
 
 export default function NotFound(){
     return(
         <>
+            <div className={styles.notFound}>
             <h1>Pagina não encontrada</h1>
-            <Link href={"/"}><a>Volta para Home</a></Link>
+            <Link href={"/"}><a className={styles.btn}>Volta para Home</a></Link>
+            </div>
         </>
     )
 
