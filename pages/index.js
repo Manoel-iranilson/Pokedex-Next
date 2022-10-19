a98ca8f8baf38016b4351404485564dc29076666
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-10-19 01:42:37 -0300

    Home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import NavBar from '../components/NavBar'
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+import Card from '../components/card'
 
-export default function Home() {
+
+export async function getStaticProps(){
+  const res = await await fetch('https://pokeapi.co/api/v2/pokemon/?limit=40')
+  const data = await res.json();
+  
+
+  data.results.forEach((item,index) => {
+    item.id = index +1
+  });
+
+  return{
+    props:{
+      pokemons: data.results
+    }
+  }
+
+}
+
+export default function Home({pokemons}) {
+  
+
   return (
-    <div className={styles.container}>
-     <h1>Hello word</h1>
-    </div>
+    
+      <>
+      <div className={styles.title_Container}>
+     <Image src="/images/pokeball.png" height="100" width="100" alt="logo"/>
+     <h1 className={styles.title}>PokeDex</h1>
+     </div>
+     <div className={styles.pokemon_container}>     
+    
+     {pokemons.map((pokemon)=>(
+      <Card key={pokemon.id} pokemon={pokemon}/>
+    ) )}
+     
+     </div>
+     </>
+    
   )
 }
